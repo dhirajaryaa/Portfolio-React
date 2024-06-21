f0a9d7e049fa6fb3df630f31a609d149768e4d3a
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2024-06-21 06:55:42 +0530

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit inventore sunt numquam, repellat facere cumque nulla magnam corrupti assumenda dolorem tempora eos ea in harum doloremque sapiente quos aut nobis.</div>
+  )
+}
+
+export default App
