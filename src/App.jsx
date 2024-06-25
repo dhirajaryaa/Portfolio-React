2d2e086e6805f99f864d70432d11278b27345df6
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2024-06-25 18:39:51 +0530

    feat: add change title of page active and not active

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import "./App.css"
 import Header from './components/header/Header';
 import Hero from './components/hero/Hero';
@@ -6,6 +6,26 @@ import About from './components/about/About';
 import Skills from './components/skills/Skills';
 
 function App() {
+  const favicon = document.getElementById("favicon");
+  useEffect(() => {
+    const handleTabFocus = () => {
+        document.title = 'Portfolio : Dhiraj Arya';
+        favicon.href = "./favicon.ico"
+    };
+    const handleTabBlur = () => {
+        document.title = 'We Miss You,back on portfolio';
+        favicon.href = "./back.ico"
+    };
+
+    window.addEventListener('focus', handleTabFocus);
+    window.addEventListener('blur', handleTabBlur);
+
+    return () => {
+        // Clean up event listeners when the component unmounts
+        window.removeEventListener('focus', handleTabFocus);
+        window.removeEventListener('blur', handleTabBlur);
+    };
+}, []);
   return (
     <>
 
