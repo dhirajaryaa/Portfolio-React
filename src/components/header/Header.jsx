e3c3ab1cde39be904723bf7a3d86879a26e21260
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2024-06-25 07:20:54 +0530

    fix: navbar code array

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,10 +3,12 @@ import "./header.css"
 
 const Header = () => {
   const [isMenuOpen,setIsMenuOpen] = useState(false);
+  const links = ["Home","About","Skills","Projects","Blog","Contact"];
 
   const handleMenu = ()=>{
     setIsMenuOpen((prev)=>!prev)
   }
+
   return (
     
     <nav className='nav'>
@@ -15,13 +17,13 @@ const Header = () => {
             <a href="#">Arya.</a>
         </div>
         <div className={isMenuOpen ? "navbar active":"navbar"}>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#skills">Skills</a></li>
-            <li><a href="#projects">Projects</a></li>
-            <li><a href="#blog">Blog</a></li>
-            <li><a href="#contact">Contact</a></li>
-            
+          {
+            links.map((value,index)=>{
+              return(
+                <li key={index}><a href={`#${value.toLocaleLowerCase()}`} onClick={handleMenu}>{value}</a></li>
+              )
+            })
+          }            
             <a href="#" className="download_cv">
               <span>Download CV</span>
             </a>
